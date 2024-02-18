888d706ee0cb21942f0a4b18a2881282e8bc18dc
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-02-18 13:12:59 +0530

    change it

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -12,12 +13,19 @@ import { app, server } from './socket/socket.js';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 app.use(errorMiddleware);
 
 server.listen(PORT, () => {
